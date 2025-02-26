afc0cd78ae27a522a9574ef42ccf86f10011f96e
Marvin <mtan2333@gmail.com>
2025-02-25 23:35:15 -0600

    Create data types

--- donations/models.py
+++ donations/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+class DonationType(models.Model):
+    name = models.CharField(max_length=30)
+
+class Donor(models.Model):
+    name = models.CharField(max_length=30)
+
+class Donation(models.Model):
+    donation_type = models.ForeignKey(DonationType, on_delete=models.CASCADE)
+    donor = models.ForeignKey(DonationType, on_delete=models.CASCADE)
+    quantity = models.IntegerField(validators=validators.MinLengthValidator)
+    date = models.DateField()
+    note = models.CharField(max_length=250)
+
+class DonationDistribution(models.Model):
+    donation = models.ForeignKey(Donation, on_delete=models.CASCADE)
+    quantity = models.IntegerField(validators=validators.MinLengthValidator)
+    date = models.DateField()
